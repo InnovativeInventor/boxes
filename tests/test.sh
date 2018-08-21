b6d2ce5133ca9b2c3b5eeab2fbfceed2d86fc0d4
InnovativeInventor <theinnovativeinventor@gmail.com>
2018-08-21 16:27:27 +0200

    Added testing for Ubuntu

--- tests/test.sh
+++ tests/test.sh
@@ -1,13 +1,16 @@
 i=0
-for d in boxes/*/ ; do (cp tests/Dockerfile $d/Dockerfile && cd $d); done
-cd boxes 
-for d in ./*/ ; do 
-    cd $d
-    monobox build || ((i++)) 
-    tag=$(echo $d | tr -cd '[:alnum:]') 
-    echo $tag
-    dgoss run --rm -d -it $tag || ((i++))
-    cd ..
+for test_env in tests/*/* ; do
+    for d in boxes/*/ ; do (cp $test_env $d/Dockerfile && cd $d); done
+    cd boxes 
+    for d in ./*/ ; do 
+        cd $d
+        monobox build || ((i++)) 
+        tag=$(echo $d | tr -cd '[:alnum:]') 
+        echo $tag
+        dgoss run --rm -d -it $tag || ((i++))
+        rm Dockerfile
+        cd ..
+    done
 done
 echo "$i"
 exit $i
