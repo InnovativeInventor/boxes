2f4295700b6e66fbc59270579f39cea2794f5558
InnovativeInventor <theinnovativeinventor@gmail.com>
2018-08-22 22:12:53 +0200

    Fixed printing issues

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,12 @@
 import glob
 import subprocess
 import json
+import colorama
+from colorama import Fore
+from colorama import Style
+import sys
+
+colorama.init()
 
 box_folders = glob.glob('boxes/**/')
 print("Boxes detected:")
@@ -10,14 +16,28 @@ test_envs = glob.glob('tests/**/*', recursive=True)
 print("Test envs detected:")
 print(test_envs)
 
+ALL_TESTS_PASSED = True
 for box in box_folders:
     with open(box + "box.json") as json_file:
         json_read = json.load(json_file)
         envs_list = json_read['tested']
         for envs in envs_list:
-            print("bash", "test.sh", envs, box)
             proc = subprocess.Popen(["bash", "tests/test.sh", envs, box], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             out, err = proc.communicate()
             exit_code = proc.wait()
-            print(out)
-            print("Error code: " + str(exit_code))
+            if exit_code is not 0:
+                ALL_TESTS_PASSED = False
+                print("bash", "test.sh", envs, box)
+                print(out.decode('utf-8'))
+                print("Error code: " + str(exit_code))
+            else:
+                print(Fore.GREEN + "Box: " + box + "     Env: " + envs)
+                print("TESTS PASSED" + Style.RESET_ALL)
+
+
+if ALL_TESTS_PASSED:
+    print('\n' + Fore.GREEN + "ALL TESTS PASSED!" + Style.RESET_ALL)
+else:
+    print('\n' + Fore.GREEN + "TESTS FAILED!" + Style.RESET_ALL)
+    sys.exit(1)
+
